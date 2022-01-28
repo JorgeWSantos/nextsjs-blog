8383224bed1b57832879e2707907ea26723a1c43
JorgeWS <jorgews.dev@gmail.com>
2022-01-28 05:48:18 -0300

    feat: 🎸 - Assets, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,29 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function 
 () {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Post</h1>
+
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
                 </Link>
             </h2>
-        </>
+
+            <Image
+                src={'/images/profile.jpg'}
+                height={144}
+                width={144}
+                alt='jorge'
+            />
+        </Layout>
     )
 }
